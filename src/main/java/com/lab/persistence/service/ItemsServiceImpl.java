e9388a5571376bcdfa40f075d7639eb758774f64
Lenovo <Lenovo@LENOVO-T420>
2017-09-06 13:59:34 +0200

    Proyecto angular con Kirby y buscador y base de datos

--- src/main/java/com/lab/persistence/service/ItemsServiceImpl.java
+++ src/main/java/com/lab/persistence/service/ItemsServiceImpl.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 import com.lab.persistence.model.ItemsModel;
 import com.lab.persistence.model.PesoModel;
 import java.util.List;
-
+import java.util.regex.Pattern;
 
 @Service
 public class ItemsServiceImpl implements ItemsService
@@ -20,6 +20,8 @@ public class ItemsServiceImpl implements ItemsService
   public List<ItemsModel> allItemsTestService(ItemsModel obj) throws Exception
   {
     List<ItemsModel> x = iMapper.allItemsTestMapper(obj);
+    
+    List<ItemsModel> list = this.replaceSearch(obj, x);
 
     return x;
   }
@@ -42,6 +44,44 @@ public class ItemsServiceImpl implements ItemsService
     List<ItemsModel> x = iMapper.numeroItemsTestMapper(obj);
 
     return x;
+  }
+
+}
+
+///////////////////////////////METODO PRIVADO
+ 
+private List<ItemsModel> replaceSearch(ItemsModel obj, List<ItemsModel> x)
+ {
+
+    for (ItemsModel item : x)
+    {
+      item.setNombre(this.pattern(obj.getNombre()).matcher(item.getNombre()).replaceAll(this.patternReplace(obj.getNombre())));
+      item.setDescripcion(this.pattern(obj.getDescripcion()).matcher(item.getDescripcion()).replaceAll(this.patternReplace(obj.getDescripcion())));
+    }
+
+    return x;
+ }
+
+/**
+   * METODO PARA GENERAR UN PATRON.
+   *
+   * @param str Cadena a insertar en el patron de busqueda.
+   * @return Patron generado.
+   */
+  private Pattern pattern(String str)
+  {
+    return Pattern.compile("(?i)" + str);
+  }
+
+  /**
+   * METODO PARA REALIZAR UN REMPLAZO DE UNA CADENA POR UN TAG HTML.
+   *
+   * @param str Cadena a insertar en el tag.
+   * @return tag generado en html.
+   */
+  private String patternReplace(String str)
+  {
+    return "<strong class=\"found\">" + str + "</strong>";
   }
 
 }
